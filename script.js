bd142821bce57c11f83745410a32f25e7fa4106f
Joy Goh-Mah <royran9er@gmail.com>
2018-03-11 01:03:06 +0000

    adds random generator button

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-var styleCopy = document.querySelector("h3");
+var styleCopy = document.querySelector(".stylecopy");
 var color1 = document.getElementById("color1");
 var color2 = document.getElementById("color2");
 var body = document.getElementById("gradient");
+var button = document.querySelector("button");
 
 function changeBackground() {
   body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
   styleCopy.textContent = body.style.background + ";";
 }
 
+function randomColor(){
+     return Math.floor(Math.random()*16777215).toString(16);
+}
+
+function randomGradient() {
+  color1.value = "#" + randomColor();
+  color2.value = "#" + randomColor();
+  changeBackground();
+}
+
 color1.addEventListener("input", changeBackground);
 
 color2.addEventListener("input", changeBackground);
+
+button.addEventListener("click", randomGradient);
